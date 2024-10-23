350a7cffb8745ca0cd9855f0a7a43d77e0c99dec
MrJake222 <norbertzpilicy@gmail.com>
2024-10-23 22:33:03 +0200

    New FPU variant (141% ice40up5k)

--- vexriscv-build/run.sh
+++ vexriscv-build/run.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 function build {
-	variant=$1
-	file=$2
+    variant=$1
+    file=$2
     src=../vexriscv-impl/src/main/scala/vexriscv/demo/$variant.scala
 
     if [[ ! -f $file || $file -ot $src ]]; then
@@ -21,6 +23,7 @@ build GenSmallestMy                 VexRiscv_sm_my.v
 build GenSmallAndProductive         VexRiscv_smprod.v
 build GenSmallAndProductiveMy       VexRiscv_smprod_my.v
 build GenSmallAndProductiveMyMulDiv VexRiscv_smprod_my_muldiv.v
+build GenSmallAndProductiveMyFpu    VexRiscv_smprod_my_fpu.v
 build GenSmallAndProductiveICache   VexRiscv_smprod_icache.v
 build GenFullNoMmuNoCache           VexRiscv_full_nommu_nocache.v
 build GenFullNoMmuNoCacheSimpleMul  VexRiscv_full_simple.v
