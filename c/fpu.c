722400b86ab4d863a7ae0b863170bf5e8320bec5
MrJake222 <norbertzpilicy@gmail.com>
2025-06-29 14:36:57 +0200

    fpu testbench with accumulation (in C)

--- c/fpu.c
+++ c/fpu.c
@@ -1,8 +1,8 @@
-#define UART0_BASE 	0x10010
-#define UART_DATA 	(*(volatile unsigned int*)(UART0_BASE + 0))
-#define UART_STATUS	(*(volatile unsigned int*)(UART0_BASE + 4))
-#define UART_HAS_RX		(UART_STATUS & (1<<0))
-#define UART_TX_EMPTY	(UART_STATUS & (1<<1))
+#define UART0_BASE  0x10010
+#define UART_DATA   (*(volatile unsigned int*)(UART0_BASE + 0))
+#define UART_STATUS (*(volatile unsigned int*)(UART0_BASE + 4))
+#define UART_HAS_RX     (UART_STATUS & (1<<0))
+#define UART_TX_EMPTY   (UART_STATUS & (1<<1))
 
 #include "printf/printf.h"
 void _putchar(char data) {
@@ -13,43 +13,54 @@ void _putchar(char data) {
 #include <math.h>
 #include <stdint.h>
 
-void test(float f1, float f2) {
-	float f3 = 0;
-	asm("dsqa %[out], %[in1], %[in2]" : [out]"=r"(f3) : [in1]"r"(f1), [in2]"r"(f2));  
-	float f3std = (f1-f2)*(f1-f2);
-	uint32_t f1u = *(uint32_t*)&f1;
-	uint32_t f2u = *(uint32_t*)&f2;
+void test(float f1, float f2, float* acc) {
+	float f3 = *acc;
+	
+	asm("dsqa %[out], %[in1], %[in2]" : [out]"+r"(f3) : [in1]"r"(f1), [in2]"r"(f2));  
+	float f3std = *acc + (f1-f2)*(f1-f2);
+
+	//float d = fabsf(f3-f3std);
+
 	uint32_t f3u = *(uint32_t*)&f3;
 	uint32_t f3stdu = *(uint32_t*)&f3std;
-	float d = fabsf(f3-f3std);
-	printf("%8.2f h%08x %8.2f h%08x %14.4f h%08x is %14.4f h%08x expt   %s %4.2f %%^3\r\n",
-	       f1, f1u, f2, f2u, f3, f3u, f3std, f3stdu, d<=0 ? "  " : "NO", d*1000000 / f3std);
+	int d = f3u < f3stdu ? f3stdu - f3u : f3u - f3stdu;
+	
+	printf_("%14.4f %08lx + (%14.4f %08lx - %14.4f %08lx)^2 = %14.4f %08lx is %14.4f %08lx expt  %s %d\r\n",
+				(double)*acc,	*(uint32_t*)acc,
+				(double)f1,		*(uint32_t*)&f1,
+				(double)f2,		*(uint32_t*)&f2,
+				(double)f3,		*(uint32_t*)&f3,
+				(double)f3std,	*(uint32_t*)&f3std,
+				d<5 ? "  " : "NO", d);
+		   
+	*acc = f3;
 }
 
 static uint32_t my_rand_seed;
 float my_rand(void) {
-    my_rand_seed = my_rand_seed * 1664525 + 1013904223;
-    return ((my_rand_seed >> 16) & 0x7FFF) / 32767.0f;
+	my_rand_seed = my_rand_seed * 1664525 + 1013904223;
+	return ((my_rand_seed >> 16) & 0x7FFF) / 32767.0f;
 }
 
 float rndf(float magnitude) {
-    return (my_rand() * 2.0f - 1.0f) * magnitude;
+	return (my_rand() * 2.0f - 1.0f) * magnitude;
 }
 
 int main(void) {
-    printf("fpu test start\r\n");
-    
-    my_rand_seed = 0;
-    for (int j=1; j<=1000; j*=10) {
+	printf("fpu test start\r\n");
+	
+	float acc = 0;
+	my_rand_seed = 0;
+	for (int j=1; j<=1000; j*=10) {
 		for (int i=0; i<10; i++) {
 			float x = rndf(j), y = rndf(j);
-			test(x, y);
+			test(x, y, &acc);
 		}
 	}
 	
 	//test(31.23f, 30.92f);
-    
-    while(1);
+	
+	while(1);
 }
 
 __attribute__ ((section(".boot")))
